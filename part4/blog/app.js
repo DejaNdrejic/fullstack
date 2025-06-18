d6a71d82ebfe1ca3c1b3102846923abb9d3265ac
Dejan Andrejic <andrejicdejan93@gmail.com>
2025-06-18 22:53:20 +0200

    4.1-4.2

---
+++ part4/blog/app.js
@@ -0,0 +1,25 @@
+const blogRouter = require('./controllers/blogs')
+const express = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const app = express()
+
+logger.info('connecting to ', config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected')
+  })
+  .catch((error) => {
+    logger.error('error when connecting to MongoDB', error.message)
+  })
+
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+app.use(middleware.reqLog)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
